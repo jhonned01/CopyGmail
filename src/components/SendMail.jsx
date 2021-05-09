3528f9e3725f1ede75c6a6e2d7b05da6e5853b11
jhonned01 <jhonned01@gmail.com>
2021-05-09 03:27:34 -0500

    iniciamos a agregar session en firebase

---
+++ src/components/SendMail.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./SendMail.css";
+import CloseIcon from "@material-ui/icons/Close";
+import Button from "@material-ui/core/Button";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { closeSendMessage } from "../features/counter/mailSlice";
+
+const SendMail = () => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="sendMail">
+      <div className="sendMail__header">
+        <h3>Nuevos Mensajes</h3>
+        <CloseIcon
+          className="sendMail__close"
+          onClick={() => dispatch(closeSendMessage())}
+        />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="To"
+          {...register("to", { required: true })}
+        />
+        {errors.to && <p className="sendMail__error">To es requerido!</p>}
+
+        <input
+          type="text"
+          placeholder="Subject"
+          {...register("subject", { required: true })}
+        />
+        {errors.subject && (
+          <p className="sendMail__error">Subject es requerido!</p>
+        )}
+
+        <input
+          type="text"
+          placeholder="Mensaje..."
+          className="sendMail__message"
+          {...register("menssage", { required: true })}
+        />
+        {errors.menssage && (
+          <p className="sendMail__error">Menssage es requerido!</p>
+        )}
+        <div className="sendMail__options">
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            className="sendMail__send"
+          >
+            Send
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SendMail;
